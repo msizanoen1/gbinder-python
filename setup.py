6fc8288b5f2d7175b813b734bde7e0b2b8e5029d
msizanoen1 <qtmlabs@protonmail.com>
2021-09-10 12:33:37 +0700

    Refactor build

--- setup.py
+++ setup.py
@@ -1,30 +1,12 @@
-import sys, subprocess
-from distutils.core import setup, Extension
+import pkgconfig
+from setuptools import setup
+from setuptools.extension import Extension
+from Cython.Build import cythonize
 
+gbinder = Extension('gbinder', sources=['gbinder.pyx'])
+pkgconfig.configure_extension(gbinder, 'libgbinder')
 
-def pkgconfig(package, kw):
-    flag_map = {'-I': 'include_dirs', '-L': 'library_dirs', '-l': 'libraries'}
-    output = subprocess.getoutput(
-        'pkg-config --cflags --libs {}'.format(package))
-    for token in output.strip().split():
-        kw.setdefault(flag_map.get(token[:2]), []).append(token[2:])
-    return kw
-
-USE_CYTHON = False
-if "--cython" in sys.argv:
-    sys.argv.remove("--cython")
-    USE_CYTHON = True
-
-file_ext = ".pyx" if USE_CYTHON else ".c"
-
-extension_kwargs = { 'sources': ["gbinder" + file_ext] }
-extension_kwargs = pkgconfig('libgbinder', extension_kwargs)
-extensions = [Extension('gbinder', **extension_kwargs)]
-
-if USE_CYTHON:
-    from Cython.Build import cythonize
-    extensions = cythonize(extensions, compiler_directives={
-                           'language_level': "3"})
+extensions = cythonize(gbinder, compiler_directives={'language_level': "3"})
 
 setup(
     name="gbinder-python",
